2293abe2cb01205f238168a21758d17c487b13d9
Florent Thévenet <florent@fthevenet.fr>
2013-03-11 08:15:15 +0100

    Fix $displayField for actors

--- app/Model/Actor.php
+++ app/Model/Actor.php
@@ -1,5 +1,8 @@
 <?php
 class Actor extends AppModel {
   public $hasAndBelongsToMany = array('Keyword', 'Project');
+  public $displayField = 'name';
+  public $virtualFields = array('name' => "CONCAT(Actor.firstName, ' ', Actor.lastName)"
+                                );
 }
 ?>
